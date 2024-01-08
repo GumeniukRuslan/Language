a8c3e40dc886b708f5b58b593deec7013dda2102
GumeniukRuslan <bishob1997@gmail.com>
2024-01-08 13:26:25 +0200

    header

---
+++ src/pages/English.jsx
@@ -0,0 +1,18 @@
+import LanguageHeader from "../components/LanguageHeader/LanguageHeader.jsx"
+
+
+const English = () => {
+
+
+  return (
+    <>
+      <LanguageHeader />
+      <main>
+    
+      </main>
+    </>
+    
+  )
+}
+
+export default English
