3ff5cac11bea3bbc61e7c5775570e977ed9a7855
GumeniukRuslan <bishob1997@gmail.com>
2024-01-13 10:13:30 +0200

    third task

--- src/components/LevelsModals/LevelsModals.jsx
+++ src/components/LevelsModals/LevelsModals.jsx
@@ -1,6 +1,7 @@
 import ModalBackdrop from "../ModalBackdrop/ModalBackdrop.jsx";
 import FirstTask from "./TaskModals/FirstTask.jsx";
 import SecondTask from "./TaskModals/SecondTask.jsx";
+import ThirdTask from "./TaskModals/ThirdTask.jsx";
 
 
 const LevelsModals = ({closeModal}) => {
@@ -8,7 +9,8 @@ const LevelsModals = ({closeModal}) => {
   return (
     <ModalBackdrop>
       {/* <FirstTask closeModal={closeModal}/> */}
-      <SecondTask closeModal={closeModal}/>
+      {/* <SecondTask closeModal={closeModal} /> */}
+      <ThirdTask closeModal={closeModal}/>
     </ModalBackdrop>
   )
 }
